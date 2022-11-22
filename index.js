80a5cc88564b470f908486ebe124c273a65e37f8
NeilMelendez <91741631+NeilMelendez@users.noreply.github.com>
2022-11-22 15:43:46 +0700

    commit

--- index.js
+++ index.js
@@ -30,13 +30,12 @@ console.log(test_date);
 
 var cors = require("cors");
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
 
 app.get("/", function (req, res) {
   res.send("Hello World");
@@ -49,6 +48,7 @@ app.get("/helloo", function (req, res) {
 });
 
 app.post("/login", async (req, res) => {
+  
   const { username, password } = req.body;
   if (!username || !password) {
     return res
